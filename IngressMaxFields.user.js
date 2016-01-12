c0d5876bb4b6863c4fc475327bfcca4bc4b4da82
Stefan Nygren <stefan.nygren@hiq.se>
2016-01-12 01:24:53 +0100

    Make c exist in right scope

--- IngressMaxFields.user.js
+++ IngressMaxFields.user.js
@@ -47,8 +47,8 @@ function wrapper() {
 	self.portalInPolygon = function portalInPolygon(polygon, portal) {
 		var poly = polygon.getLatLngs();
 		var pt = portal.getLatLng();
-
-		for (var c = false, i = -1, l = poly.length, j = l - 1; ++i < l; j = i) {
+		var c = false;
+		for (var i = -1, l = poly.length, j = l - 1; ++i < l; j = i) {
 			((poly[i].lat <= pt.lat && pt.lat < poly[j].lat) || (poly[j].lat <= pt.lat && pt.lat < poly[i].lat)) && (pt.lng < (poly[j].lng - poly[i].lng) * (pt.lat - poly[i].lat) / (poly[j].lat - poly[i].lat) + poly[i].lng) && (c = !c);
 		}
 		return c;
